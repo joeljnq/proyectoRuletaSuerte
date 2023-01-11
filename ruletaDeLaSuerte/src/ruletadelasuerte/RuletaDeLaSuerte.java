8607f5b32cd58a6a6751763fb48bee1506d5b45a
Pedro Campelo Rico <work.pedrocampelo@gmail.com>
2023-01-11 10:48:31 +0100

    Update RuletaDeLaSuerte.java

--- ruletaDeLaSuerte/src/ruletadelasuerte/RuletaDeLaSuerte.java
+++ ruletaDeLaSuerte/src/ruletadelasuerte/RuletaDeLaSuerte.java
@@ -13,8 +13,15 @@ public class RuletaDeLaSuerte {
         int limitRonda = 0; //El limite de rondas de una partida
         Jugador j1 = new Jugador("Juan", true);
 
-        System.out.println("HOLA MUNDO!!");
+        //MENU A PARTIR DE AQUI
+        System.out.println("Bienvenido a LA RULETA DE LA SUERTE");
+        System.out.println("\n              MENU\n");
+        System.out.println("1.Iniciar partida");
+        System.out.println("2.Salir");
+        System.out.println("3.Creditos(en mantenimiento)");
 
+        
+        
     }
 
 }
