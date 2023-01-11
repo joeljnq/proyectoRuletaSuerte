08a3f5c966d4b9484fe949097360562f23efd20e
JoelNQ <joelxdnq@gmail.com>
2023-01-11 09:43:28 +0100

    clase jugador

--- ruletaDeLaSuerte/src/ruletadelasuerte/Jugador.java
+++ ruletaDeLaSuerte/src/ruletadelasuerte/Jugador.java
@@ -5,5 +5,9 @@ package ruletadelasuerte;
  * @author a18jaimejnq
  */
 public class Jugador {
-
+private int comodin = 0;
+private int dinero = 0;
+private String nombre;
+private boolean turno;
+        
 }
