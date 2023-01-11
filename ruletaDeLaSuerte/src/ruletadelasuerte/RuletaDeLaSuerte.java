623335bf50f3de5387279fac8a77a5d03011f23c
JoelNQ <joelxdnq@gmail.com>
2023-01-11 09:26:28 +0100

    holaMundo

---
+++ ruletaDeLaSuerte/src/ruletadelasuerte/RuletaDeLaSuerte.java
@@ -0,0 +1,20 @@
+
+package ruletadelasuerte;
+
+/**
+ *
+ * @author a18jaimejnq
+ */
+public class RuletaDeLaSuerte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("HOLA MUNDO!!");
+    }
+
+}
