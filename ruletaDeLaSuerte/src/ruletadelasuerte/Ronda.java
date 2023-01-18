48c0b4791c1edc1e64db885e0d801d7143df4c3c
Pedro <ledtone123@gmail.com>
2023-01-18 09:52:17 +0100

    Update Ronda.java

--- ruletaDeLaSuerte/src/ruletadelasuerte/Ronda.java
+++ ruletaDeLaSuerte/src/ruletadelasuerte/Ronda.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
  * @author a18jaimejnq
  */
 public class Ronda {
-    static int rondaActual=1;
+
+    static int rondaActual = 1;
     static Scanner teclado = new Scanner(System.in, "ISO-8859-1");
     static Jugador j1 = new Jugador("Juan", true); //Nombre del jugador y si está en su turno
-    
-    public boolean finRonda(){
+
+    public boolean finRonda() {
         return true;
     }
 
-    
-    static public void menu(){
+    static public void menu() {
         //APERTURA DE MENÚ PRINCIPAL
         System.out.println("Bienvenido a LA RULETA DE LA SUERTE");
         System.out.println("\n              MENU\n----------------------------------\n");
@@ -25,7 +25,7 @@ public class Ronda {
         System.out.println("3.Creditos(en mantenimiento)");
         int eleccionOpcionMenu = teclado.nextInt();
         //CIERRE DE MENU PRINCIPAL
-        
+
         //Menú de partida
         if (eleccionOpcionMenu == 1) {
             System.out.println("\n              MENU\n----------------------------------\n");
@@ -35,35 +35,30 @@ public class Ronda {
             System.out.println("2.Resolver panel");//TODO Pedro Crear método resolverPanel
             System.out.println("3.Comprar vocal");//TODO Pedro Esto debería enlazar con j1.comprarVocal
             System.out.println("4.Salir de la partida");
-            
-            switch (eleccionOpcionMenu) {
-                case 1 -> {
+
+            do {
+                eleccionOpcionMenu = teclado.nextInt();
+                switch (eleccionOpcionMenu) {
+                    case 1 -> {
+                        //EJECUTAR EL METODO TIRAR RULETA
+                    }
+                    case 2 -> {
+                        //EJECUTAR METODO RESOLVER PANEL
+                    }
+                    case 3 -> {
+                        System.out.println("\n\nComprando vocal...");
+                        j1.comprarVocal();
+                    }
+                    case 4 -> {
+
+                    }
+                    default ->
+                        System.out.println("Elige un valor valido");
                 }
-                case 2->{
-                }
-                case 3 ->{
-                }
-                case 4 ->{
-                }
-                default -> System.out.println("Eige un valor valido");
-            }
-            //ESTAS DOS ULTIMAS SON DE PRUEBA
-            System.out.println("Comprando vocal...");
-            j1.comprarVocal();
+            } while (eleccionOpcionMenu < 1 || eleccionOpcionMenu > 4);
+
         }
-        
-      
-        
-       // System.out.println("------------------------------------------------------------------------------------");
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+        // System.out.println("------------------------------------------------------------------------------------");
     }
 }
